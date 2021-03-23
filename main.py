d98e0faeca31b4c82ee8d0e07d4e220b3d251f9e
Anastasiya <infgai@yandex.ru>
2021-03-23 13:03:32 +0300

    add form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
@@ -6,6 +6,15 @@ app=Flask(__name__)
 @app.route('/<name>')
 def index(name=None):
     return render_template('index.html') #Jinja - шаблонизированный язык https://pythonru.com/uroki/7-osnovy-shablonizatora-jinja
+
+@app.route('/forma',methods=['GET','POST'])
+def login(name=None):
+    if request.method=='GET':
+        return render_template('forma.html')
+    else:
+        name=request.form['first_form']
+    return render_template('index.html',name=name)
+
 
 host='127.0.0.1'
 port=8080
