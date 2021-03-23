b4ac7b82ecf364f1169e0788fa7149651dab94cd
Anastasiya <infgai@yandex.ru>
2021-03-23 16:30:41 +0300

    add os to host

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 
 app=Flask(__name__)
@@ -16,6 +18,6 @@ def login(name=None):
     return render_template('index.html',name=name)
 
 
-host='0.0.0.0'
-port=5000
-app.run(host,port,debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
