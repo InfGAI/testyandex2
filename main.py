80d5464320453ae2bfb18f97aec8dc26124554c6
Anastasiya <infgai@yandex.ru>
2021-03-25 19:13:22 +0300

    delete carousel, added img

--- main.py
+++ main.py
@@ -2,21 +2,46 @@ import os
 
 from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route('/')
-@app.route('/<name>')
-def index(name=None):
-    return render_template('index.html') #Jinja - шаблонизированный язык https://pythonru.com/uroki/7-osnovy-shablonizatora-jinja
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                           taste_of_day={'img': "img/table1.jpg", 'title': "Салат", 'discribe': "qqqqqqqqqqqqqqqqqqqq"})
 
-@app.route('/forma',methods=['GET','POST'])
-def login(name=None):
-    if request.method=='GET':
-        return render_template('forma.html')
-    else:
-        name=request.form['first_form']
-    return render_template('index.html',name=name)
 
+@app.route('/singin')
+def singin():
+    return render_template('singin.html')
+
+
+@app.route('/singup')
+def singup():
+    return render_template('singup.html')
+
+
+@app.route('/recipe')
+def recipe():
+    return render_template('recipe.html')
+
+
+@app.route('/foodinfo')
+def foodinfo():
+    return render_template('foodinfo.html')
+
+
+@app.route('/rest')
+def rest():
+    return render_template('rest.html')
+
+
+app.run('127.0.0.1', 8080, debug=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
